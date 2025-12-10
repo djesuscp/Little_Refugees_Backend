3fd95cad40eb598313ee34d84cc792d3216e72b4
djesuscp <djesuscruzpomares@gmail.com>
2025-12-10 12:15:42 +0100

    Fixed CORS. Dated: 2025-12-10 12:15:41

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,13 @@ import userRoutes from "./routes/userRoutes";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: [
+    'https://little-refugees-frontend.onrender.com',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Rutas principales
