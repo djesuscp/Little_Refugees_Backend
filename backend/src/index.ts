25f6cb85e6e02f43c74308f16c3eaa257293d65c
djesuscp <djesuscruzpomares@gmail.com>
2025-12-10 18:48:48 +0100

    Emergency save dated: 2025-12-10 18:48:48

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/authRoutes";
 import animalRoutes from "./routes/animalRoutes";
 import photoRoutes from "./routes/photoRoutes";
@@ -11,29 +11,10 @@ import userRoutes from "./routes/userRoutes";
 dotenv.config();
 
 const app = express();
-
-/*  ██████████████████████████████████████████████████████████████
-    🔥  CORS UNIVERSAL – FUNCIONA EN RENDER 100% GARANTIZADO
-    ██████████████████████████████████████████████████████████████ */
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Responder inmediatamente al preflight OPTIONS
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
-/*  ██████████████████████████████████████████████████████████████ */
-
+app.use(cors());
 app.use(express.json());
 
-// Rutas API
+// Rutas principales
 app.use("/api/auth", authRoutes);
 app.use("/api/animals", animalRoutes);
 app.use("/api/photos", photoRoutes);
@@ -43,4 +24,5 @@ app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
 
